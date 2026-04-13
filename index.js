99926ee6ceb1385396db17467ef4b354545e41f7
Litzy Reyes <litzyreyes1807@gmail.com>
2026-04-13 16:50:03 -0500

    public index

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-// app.use(express.static('public'));
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+// });
+ app.use(express.static('public'));
 
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
