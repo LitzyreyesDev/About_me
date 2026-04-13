27743de5eaa8a8879b80b9fee785cbbf0a425fe8
Litzy Reyes <litzyreyes1807@gmail.com>
2026-04-13 16:34:33 -0500

    add message

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-
-app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+// app.use(express.static('public'));
 
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
